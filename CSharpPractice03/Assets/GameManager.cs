a8aab74174e58973fc514798047d60f6e4148c80
pikagamed <51389229+pikagamed@users.noreply.github.com>
2019-06-12 11:12:07 +0800

    UPDATE
    基本船艦設定功能

---
+++ CSharpPractice03/Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Ship ship01 = new Ship();  //實作一個Ship物件，命名為ship
+    public Ship ship02 = new Ship( 27.5M, 5.2M, 4.8M);  //實作一個Ship物件，命名為ship
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ship01.knot = 10.5M;                                          //寫入欄位
+        Debug.Log("船艦01的速度：" + ship01.knot);        //讀取欄位
+
+        ship01.length = 30.8M;                                        //寫入屬性
+        Debug.Log("船艦01的長度：" + ship01.length);      //讀取屬性
+
+        //ship.width = 4.3f;                                        //唯讀屬性不可寫
+        Debug.Log("船艦01的寬度：" + ship01.width);      //讀取唯讀屬性
+
+        Debug.Log("船艦01的高度：" + ship01.height);
+        Debug.Log("船艦01的體積：" + ship01.volume);
+
+
+        Debug.Log("船艦02的速度：" + ship02.knot);  
+        Debug.Log("船艦02的長度：" + ship02.length); 
+        Debug.Log("船艦02的寬度：" + ship02.width);  
+        Debug.Log("船艦02的高度：" + ship02.height);
+        Debug.Log("船艦02的體積：" + ship02.volume);
+    }
+}
--- CSharpPractice03/Assets/Ship.cs
+++ CSharpPractice03/Assets/Ship.cs
@@ -1,18 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using UnityEngine;
 
-public class Ship : MonoBehaviour
+public class Ship 
 {
-    // Start is called before the first frame update
-    void Start()
+    #region 欄位、屬性
+    //欄位
+    public decimal knot;  //船的速度海浬／小時≡節來定義
+
+    //屬性
+    public decimal length { get; set; }
+    //唯讀屬性
+    //public float width { get; }
+
+    //唯寫屬性，但自動實作的屬性必須有get存取子
+    //public string password { set; }
+
+    //封裝欄位
+    private decimal _width = 4.3M;
+    public decimal width { get { return _width; } set { _width = value; } }
+
+    private decimal _height = 3.4M;
+    public decimal height { get => _height; set => _height = value; }       //使用λ運算子
+
+    private decimal _volume = 450.296M;
+    public decimal volume
     {
-        
+        get
+        {
+            _volume = length * width * height;
+            return _volume;
+        }
+    }
+    #endregion
+
+    /// <summary>
+    /// 類別Ship建構子
+    /// </summary>
+    /// <param name="length">船艦長度</param>
+    /// <param name="width">船艦寬度</param>
+    /// <param name="height">船艦高度</param>
+    public Ship(decimal length, decimal width, decimal height)
+    {
+        this.length = length;
+        this.width = width;
+        this.height = height;
     }
 
-    // Update is called once per frame
-    void Update()
+    public Ship()
     {
-        
+        this.length = 30.8M;
+        this.width = 4.3M;
+        this.height = 3.4M;
     }
 }
